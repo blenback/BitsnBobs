0ce43b32c8dad207f666d035366322d00e04d041
blenback <bblack@ethz.ch>
2022-12-29 10:56:15 +0100

    remove webhook

--- slack_communication.R
+++ slack_communication.R
@@ -1,12 +1,12 @@
 
-create_config_file(token = 'xapp-1-A049C8Z8ALW-4318425761910-7988238c82da2316a2c87bee2ad1962d6e42792243edd7517b12cce53a73056f',
-  incoming_webhook_url = 'https://hooks.slack.com/services/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',
+create_config_file(token = 'test' ,
+  incoming_webhook_url = 'test',
   channel = '#general',
   username = 'slackr',
   icon_emoji = 'tada')
 
 slackr_setup(channel = '#general',
-             incoming_webhook_url = 'https://hooks.slack.com/services/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')
+             incoming_webhook_url = 'test')
 
 #create a test promise
 promise <- promises::future_promise(read.csv("Tools/Simulation_control.csv"))
